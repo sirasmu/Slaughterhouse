87f9d90ad849ff508bee3e5dbc375203fc9e7ab7
S. Rasmussen <siras@outlook.com>
2017-11-18 13:07:53 +0100

    Add files via upload

---
+++ tier3/DataController.java
@@ -0,0 +1,43 @@
+package tier3;
+
+import org.omg.PortableServer.AdapterActivatorOperations;
+
+import common.model.Animal;
+import common.model.AnimalCollection;
+import common.model.PackageCollection;
+import common.model.TrayCollection;
+
+public class DataController
+{
+	IDataAdapter adapter;
+	
+	public DataController (IDataAdapter adapter)
+	{
+		this.adapter = adapter;
+	}
+	
+	public void saveAnimals(AnimalCollection animals)
+	{
+		adapter.saveAnimals(animals);
+	}
+	
+	public void saveTrays(TrayCollection trays)
+	{
+		adapter.saveTrays(trays);
+	}
+	
+	public void savePackages(PackageCollection packages)
+	{
+		adapter.savePackages(packages);
+	}
+	
+	public void saveBadPackages(PackageCollection packages)
+	{
+		adapter.saveBadPackages(packages);
+	}
+	
+	public PackageCollection getBadPackages(PackageCollection packages)
+	{
+		return adapter.getBadPackages(packages);
+	}	
+}
