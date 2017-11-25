772af42ae0ae9fb2e586d0f05b4f4398543312e5
S. Rasmussen <siras@outlook.com>
2017-11-25 21:43:19 +0100

    Update WebServiceManager.java
    Hi I had a merge conflict so I will just upload this here so you also can see it. I have some other changes too.

--- FULLPROJECT/Tier2Project/src/tier2/webservice/WebServiceManager.java
+++ FULLPROJECT/Tier2Project/src/tier2/webservice/WebServiceManager.java
@@ -1,6 +1,9 @@
 package tier2.webservice;
 
+import java.util.ArrayList;
 import common.model.Animal;
+import common.model.PartType;
+import common.model.TrayCollection;
 import tier2.Controller.RegisterSystemController;
 
 public class WebServiceManager implements ISlaughterhouseWebService
@@ -28,36 +31,91 @@ public class WebServiceManager implements ISlaughterhouseWebService
 		String messageType = getMessageType(message);
 		switch(messageType)
 		{
-			case WebServiceConfig.REGISTER_ANIMAL:
-				Animal animal = parseToAnimal(message);
-				controller.registerAnimal(animal);
-				return "Animal registered: " + animal;
+			/* Animal client */
+			case WebServiceConfig.REGISTER_ANIMAL: // REGISTER_ANIMAL 14.0
+				controller.registerAnimal(parseMessageToAnimal(message));
+				return "\nAnimal registered";
+			/* Tray Client */
 			case WebServiceConfig.REQUEST_UNCUT_ANIMALS:
-			case WebServiceConfig.ADD_PART_TO_TRAY:
-			case WebServiceConfig.REQUEST_TRAYS_READY_FOR_PACKAGING:
-			case WebServiceConfig.REGISTER_PACKAGE:
-				//Translate message into animal object;
-				//Pass object to controller....... wait does the controller need to be a singleton?
+				
+			case WebServiceConfig.ADD_PART_TO_TRAY: // ADD_PART_TO_TRAY 97c5134f-13e7-4d0c-8bd3-1d5cf5e0036a weights:2.0,3.3,9.5
+				controller.splitAnimal(parseMessageToAnimalById(message), parseMessageToPartsWeights(message));
+				return "\nAnimal was split into parts of different weights";
+			/* PackClient */
+			case WebServiceConfig.REQUEST_TRAYS_READY_FOR_PACKAGING: // REQUEST_TRAYS_READY_FOR_PACKAGING HAM
+				TrayCollection trays = getReadyTraysByType(message);
+				return responseWithTraysById(trays); // RESPONSE_TRAYS_READY_FOR_PACKAGING trays:97c5134f-13e7-4d0c-8bd3-1d5cf5e0036a,97c5134f-13e7-4d0c-8bd3-1d5cf5e0036a
+			case WebServiceConfig.REGISTER_PACKAGE: // REGISTER_PACKAGE trays:
+				//AbstractPackage pk = getReadyTraysByType(message)
+				//AbstractPackage pk = new PackageHalfAnAnimal(new TrayCollection());
+				//AbstractPackage pk = new PackageByType(new TrayCollection(), "type");
+				//controller.registerPackage(pk); 
 				return "";
-
 			default: 
 				return "You contacted the server, but it could not intepret the content";
 		}
 	}
 	
-	private Animal parseToAnimal(String message)
+	private String responseWithTraysById(TrayCollection trays)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String response = WebServiceConfig.RESPONSE_TRAYS_READY_FOR_PACKAGING;
+		response += " trays:";
+		for(int i = 0; i < trays.getTrayCollection().size(); i++){
+			if(i < trays.getTrayCollection().size() -1){
+				response += trays.getTrayCollection().get(i) + ",";
+			}
+			else
+			{
+				response += trays.getTrayCollection().get(i);
+			}
+		}
+		
+		return response;
+	}
+	
+	private TrayCollection getReadyTraysByType(String message)
+	{
+		String[] splitMessage = message.split(" ");
+		String type = splitMessage[1];
+		switch(type)
+		{
+			case PartType.HAM:
+				return controller.getHamTraysReadyForPackaging();
+			case PartType.LEG:
+				return controller.getLegTraysReadyForPackaging();
+			default:
+				throw new IllegalArgumentException();
+		}		
+	}
+
+	private ArrayList<Double> parseMessageToPartsWeights(String message)
+	{
+		String[] splitMessage = message.split(":");
+		String[] weightArray = splitMessage[1].split(",");
+		ArrayList<Double> list = new ArrayList<>();
+		for(String weight: weightArray){
+			list.add(Double.parseDouble(weight));
+		}
+		return list;
+	}
+
+	private Animal parseMessageToAnimalById(String message)
+	{
+		String[] splitMessage = message.split(" ");
+		String id = splitMessage[1];
+		return controller.getAnimals().findAnimalById(id);
+	}
+
+	private Animal parseMessageToAnimal(String message)
+	{
+		String[] splitMessage = message.split(" ");
+		double weight = Double.parseDouble(splitMessage[1]);
+		return new Animal(weight);
 	}
 
 	private String getMessageType(String message)
 	{
 		String[] splitMessage = message.split(" ");
 		return splitMessage[0];	
-	}
-
-
-	
-	
+	}	
 }
