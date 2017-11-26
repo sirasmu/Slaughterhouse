8941e7f272a5d087d33e20dd4de4a899ddb5dd52
S. Rasmussen <siras@outlook.com>
2017-11-26 18:21:31 +0100

    Add files via upload

---
+++ FULLPROJECT/Tier1Project/SlaughterhouseAnimalClient/SlaughterhouseAnimalClient/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlaughterhouseAnimalClient
+{
+
+   public partial class AnimalClient : Form
+   {
+      private Slaughterhouse.ISlaughterhouseWebService webService = new Slaughterhouse.ISlaughterhouseWebService();
+
+      public AnimalClient()
+      {
+         InitializeComponent();
+      }
+
+      private void button1_Click( object sender, EventArgs e )
+      {
+         try
+         {
+            textBox2.Text = webService.readIncomingMessage(string.Format("REGISTER_ANIMAL {0}", textBox1.Text));
+         }
+         catch (Exception ex)
+         {
+            textBox2.Text =  "StackTrace:" + ex.StackTrace;
+         }
+      }
+   }
+}
